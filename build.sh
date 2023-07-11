5280ada70790bec196ffb85f1d71ee3c38499321
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-07-11 20:57:10 +0200

    wip

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Uninstall the Python package using pip
-yes | pip uninstall robin
+yes | pip uninstall robingrad
 
 # Read the version from pyproject.toml and store it in a variable
 version=$(awk -F'"' '/version =/ {print $2}' pyproject.toml)
@@ -13,8 +13,8 @@ if [ -d "dist" ]; then
   rm -rf dist
 fi
 
-if [ -d "robin.egg-info" ]; then
-  rm -rf robin.egg-info
+if [ -d "robingrad.egg-info" ]; then
+  rm -rf robingrad.egg-info
 fi
 
 # build the package
@@ -22,7 +22,7 @@ python -m build
 
 # install builded package 
 cd dist
-command="pip install robin-${version}.tar.gz"
+command="pip install robingrad-${version}.tar.gz"
 eval $command
 
 
