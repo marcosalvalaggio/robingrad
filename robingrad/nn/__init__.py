049ba595f0ef516b0251eecacfd315e99589c27e
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-07-12 18:33:35 +0200

    wip

--- robingrad/nn/__init__.py
+++ robingrad/nn/__init__.py
@@ -6,5 +6,18 @@ class Linear:
         self.weight = Tensor.normal(0,1,(out_features, in_features))
         self.bias = Tensor.normal(0,1,(out_features,)) if bias else None
     
-    def __call_(self, x):
+    def __call_(self, x: "Tensor") -> "Tensor":
         return x
+    
+
+
+# class Linear:
+#     def __init__(self, in_features: int, out_features: int, bias: bool = True):
+#         self.weight = Tensor.normal(0, 1, (out_features, in_features))
+#         self.bias = Tensor.normal(0, 1, (out_features,)) if bias else None
+    
+#     def __call__(self, x: Tensor) -> Tensor:
+#         out = x @ self.weight.T  # Matrix multiplication
+#         if self.bias is not None:
+#             out += self.bias
+#         return out
