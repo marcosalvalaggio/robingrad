85d3a5dc149760e710091f2c1c7b5292447c53d4
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-07-12 21:13:45 +0200

    wip

--- README.md
+++ README.md
@@ -7,5 +7,5 @@
 
 [![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://GitHub.com/Naereen/StrapDown.js/graphs/commit-activity) [![stability-wip](https://img.shields.io/badge/stability-wip-lightgrey.svg)](https://github.com/mkenney/software-guides/blob/master/STABILITY-BADGES.md#work-in-progress)
 
-Something between George Hotz [Tinygrad](https://github.com/tinygrad/tinygrad) and Andrej karpathy [Micrograd](https://github.com/karpathy/micrograd).
+Something between [Tinygrad](https://github.com/tinygrad/tinygrad) and [Micrograd](https://github.com/karpathy/micrograd).
 
--- robingrad/nn/__init__.py
+++ robingrad/nn/__init__.py
@@ -3,13 +3,15 @@ from robingrad.tensor import Tensor
 
 class Linear:
     def __init__(self, in_features: int, out_features: int, bias: bool = True):
-        self.weight = Tensor.normal(0,1,(out_features, in_features))
+        self.weight = Tensor.normal(0,1,(in_features, out_features)) # not (out, in) in this way I don't need to transpose the weight matrix
         self.bias = Tensor.normal(0,1,(out_features,)) if bias else None
     
     def __call_(self, x: "Tensor") -> "Tensor":
-        return x
+        out = x @ self.weight
+        if self.bias:
+            out += self.bias
+        return out
     
-
 
 # class Linear:
 #     def __init__(self, in_features: int, out_features: int, bias: bool = True):
