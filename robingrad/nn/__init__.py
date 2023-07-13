e8858d8874d49a44511934795499d53b7010c35a
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-07-13 18:40:33 +0200

    wip

--- robingrad/nn/__init__.py
+++ robingrad/nn/__init__.py
@@ -4,7 +4,7 @@ from robingrad.tensor import Tensor
 class Linear:
     def __init__(self, in_features: int, out_features: int, bias: bool = True):
         self.weight = Tensor.normal(0,1,(out_features, in_features), requires_grad=True)
-        self.bias = Tensor.normal(0,1,(out_features), requires_grad=True) if bias else None
+        self.bias = Tensor.normal(0,1,(1,out_features), requires_grad=True) if bias else None
     
     def __call__(self, x: "Tensor") -> "Tensor":
         return x.linear(self.weight.T, self.bias)
