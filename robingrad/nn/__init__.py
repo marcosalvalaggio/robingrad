2a0aab741059a120d8cbb757a679ec341e051fa4
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-07-13 17:44:18 +0200

    wip

--- robingrad/nn/__init__.py
+++ robingrad/nn/__init__.py
@@ -3,12 +3,9 @@ from robingrad.tensor import Tensor
 
 class Linear:
     def __init__(self, in_features: int, out_features: int, bias: bool = True):
-        self.weight = Tensor.normal(0,1,(in_features, out_features), requires_grad=True) # By inverting in and out num, I don't need to transpose the weight matrix.
-        self.bias = Tensor.normal(0,1,(out_features,), requires_grad=True) if bias else None
+        self.weight = Tensor.normal(0,1,(out_features, in_features), requires_grad=True)
+        self.bias = Tensor.normal(0,1,(out_features), requires_grad=True) if bias else None
     
     def __call__(self, x: "Tensor") -> "Tensor":
-        out = x @ self.weight
-        if self.bias:
-            out += self.bias
-        return out
+        return x.linear(self.weight.T, self.bias)
     
