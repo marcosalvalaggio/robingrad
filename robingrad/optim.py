3fae65b5cce94fc5abaf7f602c58c0c98139a9cb
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-07-17 18:00:04 +0200

    wip

--- robingrad/optim.py
+++ robingrad/optim.py
@@ -1,20 +1,19 @@
-from tinygrad.state import get_parameters
+from tinygrad.tensor import Tensor
 import numpy as np
-from typing import TypeVar
-
-ModelType = TypeVar('ModelType')
+from typing import List
 
 class Optimizer:
-    def __init__(self, model: ModelType):
-        self.params = get_parameters(model)
+    def __init__(self, params: List[Tensor]):
+        self.params = params
 
     def zero_grad(self) -> None:
-        for param in self.params: param.grad = np.zeros_like(param.grad)
+        for param in self.params: 
+            param.grad = np.zeros_like(param.grad)
         
 
 class SGD(Optimizer):
-    def __init__(self, model: ModelType, lr: float = 3e-4):
-        super().__init__(model=model)
+    def __init__(self, params: List[Tensor], lr: float = 3e-4):
+        super().__init__(params)
         self.lr = lr
 
     def step(self) -> None:
@@ -24,8 +23,8 @@ class SGD(Optimizer):
 
 # https://pytorch.org/docs/stable/generated/torch.optim.Adam.html
 class Adam(Optimizer):
-    def __init__(self, model: ModelType, lr: float = 0.0001, beta1: float = 0.9, beta2: float =  0.999, eps: float = 1e-08):
-        super().__init__(model=model)
+    def __init__(self, params: List[Tensor], lr: float = 0.0001, beta1: float = 0.9, beta2: float =  0.999, eps: float = 1e-08):
+        super().__init__(params)
         self.lr, self.beta1, self.beta2, self.eps = lr, beta1, beta2, eps
         self.first_moments = [np.zeros_like(param.data) for param in self.params]
         self.second_moments = [np.zeros_like(param.data) for param in self.params]
