9a407fecc306e07ce1cf2418e259f72202643758
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-07-31 17:54:13 +0200

    wip

--- robingrad/nn/loss.py
+++ robingrad/nn/loss.py
@@ -5,3 +5,8 @@ class MSELoss:
     def __call__(self, output: Union[List[Tensor], Tensor], target: Union[List[Tensor], Tensor]) -> Tensor:
         return ((output - target)**2).mean()
         
+
+class BCELoss:
+    def __call__(self, output: Union[List[Tensor], Tensor], target: Union[List[Tensor], Tensor]) -> Tensor:
+        neg_output = 1-output
+        return (-(target * output.log() + (1-target)*neg_output.log())).mean()
